149f86ddecf91118e0512818e3d18cfe69b45586
Yi Chen <yichen@gmail.com>
2012-09-24 23:54:35 -0700

    refactor

--- shi.js
+++ shi.js
@@ -41,12 +41,12 @@
     return util.format('%s/%s/%s', year, toString(month), toString(day));
   };
 
-  shi.year = {
-    toNumber : function (y) {
+  var Year = shi.year = function (y) {
       var year = _.isNumber(y) ? y : parseInt(y, 10);
 
-      return (year > 2000) ? year : 2000 + year;
-    }
+      (year > 2000) || (year += 2000);
+
+      return this._year = year;
   };
 
   var Month = shi.month = function (m) {
@@ -75,29 +75,9 @@
     }
   });
 
-  // shi.month = {
-  //   toNumber : function (m) { 
-  //     if (_.isNumber(m)) return m;
-
-  //     if (m[0] > '0' && m[0] <= '9') return parseInt(m, 10);
-
-  //     var s = m.toLowerCase();
-  //     var result = 0;
-  //     var i = 0;
-
-  //     _.each(months, function (month) {    
-  //       if (_.indexOf(month, s) !== -1) result = i;
-  //       else i += 1;
-  //     });
-
-  //     if (i===12) return -1;
-  //     else return result+1; 
-  //   }
-  // };
-
-  shi.day = {
-    toNumber : function(d) {
-      return _.isNumber(d) ? d : parseInt(d, 10);
-    }
+  var Day = shi.day = function (d) {
+    _.isNumber(d) || (d = parseInt(d, 10));
+    return this._day = d;
   };
+
 }).call(this);
